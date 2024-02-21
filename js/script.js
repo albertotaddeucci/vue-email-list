f0d843d6a2d95432cb27167aaee41f1d42f5bcf4
alberto <alberto.taddeucci@gmail.com>
2024-02-21 14:57:07 +0100

    lista email completata

--- js/script.js
+++ js/script.js
@@ -3,10 +3,22 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'hello'
+        emails: []
       }
     },
     mounted(){
+
+        //ottengo 10 email che inserisco in array emails
+        for(let i = 0; i<=10; i++){
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {           
+    
+            this.emails.push(response.data.response)
+    
+        });
+            
+        }
        
     }
   }).mount('#app')
