6ad075390aa146e4a96340be0ce5770d137ab86e
alberto <alberto.taddeucci@gmail.com>
2024-02-21 15:01:49 +0100

    fix email

--- js/script.js
+++ js/script.js
@@ -9,16 +9,18 @@ const { createApp } = Vue
     mounted(){
 
         //ottengo 10 email che inserisco in array emails
-        for(let i = 0; i<=10; i++){
+        for(let i = 0; i<10; i++){
 
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(response => {           
     
             this.emails.push(response.data.response)
+            console.log(this.emails.length)
     
         });
             
-        }
-       
+    }
+    
     }
   }).mount('#app')
+
